432803fbf765de4da78fa95cdd92b0e087c9af26
Frédéric Delorme <frederic.delorme@gmail.com>
2020-06-29 22:34:43 +0200

    using hook useCallback

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-import {render} from 'react-dom'
+import React, { useState, useEffect, useMemo, useCallback } from 'react'
+import { render } from 'react-dom'
 
-import App from './App'
+function wait(duration) {
+    const t = Date.now()
+    while (true) {
+        if (Date.now() - t > duration) {
+            return true
+        }
+    }
+}
 
-import './index.css'
+const Button = React.memo(function ({ onClick }) {
+    console.log('render')
+    return <button onClick={onClick}>Mon Bouton</button>
+})
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    const handleClick = useCallback(function () {
+        alert('bonjour')
+    }, [])
+
+    return <div>
+        <Button onClick={handleClick} />
+        <button onClick={() => setCount(c => c + 1)}>Incrémenter {count}</button>
+    </div>
+}
 
 render(
- <div><App/></div>,
- document.getElementById("app")
- )
+    <App />,
+    document.getElementById("app")
+)
